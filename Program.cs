6cf3b5a1ae71c2d5c84b984a1c7f246bbe034e23
eldar23451 <liyev8024@gmail.com>
2024-05-17 22:09:42 +0400

    task file

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int Sum()
+//{
+//    Console.WriteLine("Please enter first number");
+//    int number=int.Parse(Console.ReadLine());
+//    Console.WriteLine("please enter second number");-RETURN TYPE OLAN VE PARAMETRLERI OLMAYAN
+//    int number2=int.Parse(Console.ReadLine());
+//    int res=number+number2;
+//    return res;
+
+
+
+//}Console.WriteLine(Sum());
+//int Quvve(int num1, int num2)
+//{
+//    return num1^num2;
+
+
+
+//}Console.WriteLine(Quvve(2, 5));
+
